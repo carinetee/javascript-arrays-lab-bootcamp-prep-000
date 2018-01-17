56dbe06efd53b956885e4024c133ad144e995229
Carine Tomassian <carinetomassian@gmail.com>
2018-01-17 07:47:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,6 +21,6 @@ function destructivelyRemoveFirstKitten() {
 }
 
 function appendKitten(name) {
-  var addKitten = (...kittens, name);
-  retun addKitten;
+  var addKitten = []...kittens, name];
+  return addKitten;
 }
